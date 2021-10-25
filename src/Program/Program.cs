5fbd9c370ac5f0527f613d3dcc679bdad4646258
Salonso1602 <santiago.ralonso@correo.ucu.edu.uy>
2021-10-25 19:17:00 -0300

    creado arboles de 8 personas

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,22 +7,25 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Node n1 = new Node(1);
-            Node n2 = new Node(2);
-            Node n3 = new Node(3);
-            Node n4 = new Node(4);
-            Node n5 = new Node(5);
-            Node n6 = new Node(6);
-            Node n7 = new Node(7);
+            Node n1 = new Node("Daniel",53);
+            Node n2 = new Node("Mercedes",51);
+            Node n3 = new Node("Lionel",36);
+            Node n4 = new Node("Rafael",19);
+            Node n5 = new Node("David",23);
+            Node n6 = new Node("Pablo",27);
+            Node n7 = new Node("Lara",8);
+            Node n8 = new Node("Isabel",3);
 
-            n1.AddChildren(n2);
             n1.AddChildren(n3);
+            n1.AddChildren(n5);
 
-            n2.AddChildren(n4);
+            n2.AddChildren(n3);
             n2.AddChildren(n5);
 
-            n3.AddChildren(n6);
+            n3.AddChildren(n4);
             n3.AddChildren(n7);
+            n3.AddChildren(n8);
+
 
             // visitar el árbol aquí
         }
