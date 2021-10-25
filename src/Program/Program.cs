5c25ef788ac7d39ae7d1968e9b574ea2e32a9f89
Salonso1602 <santiago.ralonso@correo.ucu.edu.uy>
2021-10-25 19:19:20 -0300

    terminado ejercicio 2

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -18,9 +18,11 @@ namespace Program
 
             n1.AddChildren(n3);
             n1.AddChildren(n5);
+            n1.AddChildren(n6);
 
             n2.AddChildren(n3);
             n2.AddChildren(n5);
+            n2.AddChildren(n6);
 
             n3.AddChildren(n4);
             n3.AddChildren(n7);
